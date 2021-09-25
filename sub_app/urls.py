218dd5c6ed3f33cc5e17817f1ebe002816cb6b11
Shayan Debroy <65186501+shayan-cyber@users.noreply.github.com>
2021-09-25 12:52:17 +0530

    PIN removed

--- sub_app/urls.py
+++ sub_app/urls.py
@@ -4,5 +4,7 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('select_location', views.select_location, name="select_location"),
     path('get_route/<str:center_id>/<str:pincode>/<str:lat>/<str:longi>/<str:cur_lat>/<str:cur_longi>', views.get_route, name="get_route"),
+    path('about', views.about, name="about"),
+    
 
 ]
--- sub_app/views.py
+++ sub_app/views.py
@@ -8,30 +8,15 @@ import datetime
 
 # Create your views here.
 def home(request):
-    # pin = 781034
-    # date = '23-09-2021'
-    # COWIN_URL = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode={pin}&date={date}"
-    # print(COWIN_URL)
-    # resp = requests.get(COWIN_URL)
-    # # print(resp.json())
-    # # print(json.loads(resp))
-    # resp_json = resp.json()
-    # for i in resp_json['sessions']:
-    #     print(i)
-    #     print('\n')
-    #     print(i['lat'])
-
-
     context = {
-        # 'resp':resp.json(),
+        
     }
-
     return render(request, 'index.html',context )
 def select_location(request):
     
     if request.method == 'POST':
         pos = request.POST.get('pos', '')
-        pin = request.POST.get('pin', '')
+        # pin = request.POST.get('pin', '')
         type_ = request.POST.get('type', '')
         print(type_)
         
@@ -52,32 +37,24 @@ def select_location(request):
             'accept': 'application/json',
             'Accept-Language': 'hi_IN'
             }
-            print(lat_long_url)
+            
             resp = requests.get(lat_long_url ,headers=headers)
             
             resp_json = resp.json()
-        
-            # for i in resp_json['sessions']:
-            #     print(i)
-            #     print('\n')
-            #     print(i['lat'])
             centers = resp_json['centers']
-            # print(centers)
             centers_list = {}
+
+
             for i in centers:
-                # print(i)
-                # print('\n')
                 centers_list[str(i['name'])] = {
                     'center_id':i['center_id'],
                     'pincode':i['pincode'],
                     'lat':i['lat'],
                     'lng':i['long'],
                 }
-                # centers_list.append(i)
+                
             
-            # print("list:")
-            # print("\n")
-            # print(centers_list)
+            
             context ={
                 'centers_list':centers_list,
                 'centers':resp_json,
@@ -85,66 +62,15 @@ def select_location(request):
                 'longi':longi
 
             }
-        elif type_ == 'pin':
-            print(pin)
-            today= datetime.date.today()
-            d1 = today.strftime("%d-%m-%Y")
-            print(d1)
-            # date = '23-09-2021'
-            
-            COWIN_URL = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode={pin}&date={d1}"
-            print(COWIN_URL)
-            resp = requests.get(COWIN_URL)
-            # print(resp.json())
-            # print(json.loads(resp))
-            resp_json = resp.json()
-            centers_list = {}
-            print(resp_json)
-            for i in resp_json['sessions']:
-                # print("center")
-                # print("\n")
-                # print(i)
-                # print('\n')
-                centers_list[str(i['name'])] = {
-                    'center_id':i['center_id'],
-                    'pincode':i['pincode'],
-                    'lat':i['lat'],
-                    'lng':i['long'],
-                }
-                # print(i['lat'])
-            print(centers_list)
-            context ={
-                'centers_list':centers_list,
-                'centers':resp_json,
-                'lat':lat,
-                'longi':longi,
-                'pin':True,
 
-            }
-
-
-
-
-
+       
     return render(request, 'select_location.html', context)
 
 def get_route(request,center_id,pincode, lat, longi, cur_lat, cur_longi):
-    print(center_id)
-    print(pincode)
-    print(lat)
-    print(longi)
-    print(cur_lat)
-    print(cur_longi)
-
     today= datetime.date.today()
     d1 = today.strftime("%d-%m-%Y")
-    print(d1)
-    # date = '23-09-2021'
-    
     COWIN_URL = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode={pincode}&date={d1}"
-    print(COWIN_URL)
     resp = requests.get(COWIN_URL)
-   
     resp_json = resp.json()
     details = {}
    
@@ -158,11 +84,6 @@ def get_route(request,center_id,pincode, lat, longi, cur_lat, cur_longi):
             details['Available capacity dose one'] = i['available_capacity_dose1']
             details['Available capacity dose two'] = i['available_capacity_dose2']
             details['Vaccine'] = i['vaccine']
-
-
-    # for i in centers:
-    #     if i['center_id'] == center_id:
-    #         pass
     print(details)
     context ={
         'start_lat':lat,
@@ -171,7 +92,9 @@ def get_route(request,center_id,pincode, lat, longi, cur_lat, cur_longi):
         'end_long':cur_longi,
         'details':details,
     }
-
-
-    
     return render(request, 'get_route.html', context)
+
+
+def about(request):
+
+    return render(request, 'about.html')
